57361314c716e7f7e155ffb9c45070d5468ee38f
nazephyrus <126683488+naden01@users.noreply.github.com>
2026-01-03 21:33:24 +0700

    initial tree X6525B

---
+++ twrp_X6525B.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from Infinix-X6525B device
+$(call inherit-product, device/infinix/X6525B/device.mk)
+
+PRODUCT_DEVICE := X6525B
+PRODUCT_NAME := twrp_X6525B
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X6525B
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
